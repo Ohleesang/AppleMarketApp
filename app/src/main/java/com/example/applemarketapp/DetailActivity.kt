9da3bae3fb581bac8ec0f6f51aef3ae3840b92b8
Ohleesang <jammmmed@gmail.com>
2024-01-07 11:36:03 +0900

    Feat : 하트를 누를때마다 image 변경

--- app/src/main/java/com/example/applemarketapp/DetailActivity.kt
+++ app/src/main/java/com/example/applemarketapp/DetailActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
+import android.widget.Toast
 import com.example.applemarketapp.data.Item
 import com.example.applemarketapp.databinding.ActivityDetailBinding
 import com.example.applemarketapp.databinding.ActivityMainBinding
@@ -17,6 +18,8 @@ class DetailActivity : AppCompatActivity() {
         setDataView()
 
         backBtnOnClick()
+
+        heartOnClick()
 
     }
 
@@ -38,15 +41,51 @@ class DetailActivity : AppCompatActivity() {
         //text에 언더라인 추가
         val degreeTv = binding.mannerDegreeTv
         val underlineText = SpannableString(degreeTv.text)
-        underlineText.setSpan(UnderlineSpan(),0,underlineText.length,0)
+        underlineText.setSpan(UnderlineSpan(), 0, underlineText.length, 0)
         degreeTv.text = underlineText
 
     }
+
     //뒤로가기 버튼 이벤트 설정
-    private fun backBtnOnClick(){
+    private fun backBtnOnClick() {
         binding.backBtnIv.setOnClickListener {
             finish()
         }
 
+    }
+
+    private fun heartOnClick() {
+        val heartIv = binding.heartIv
+
+        heartIv.setOnClickListener {
+
+            //tag 값에 리소스 값을 저장하여 비교
+
+            val onHeartResId = R.drawable.img_all_like
+            val offHeartResId = R.drawable.img_all_no_like
+
+            //처음 tag가 null이면 최초 상태이므로
+            val heartTag = it.tag as? Int ?: offHeartResId
+
+
+
+            if (heartTag == offHeartResId) { // 하트 Off
+                //1. img 변경
+                heartIv.setImageResource(onHeartResId)
+                it.tag = onHeartResId
+
+                //2. 좋아요 수 증가
+
+                //3. 스낵바 표시
+
+            } else if (heartTag == onHeartResId) { // 하트 On
+                //1. img 변경
+                heartIv.setImageResource(offHeartResId)
+                it.tag = offHeartResId
+
+                //2. 좋아요 수 감소
+            }
+
+        }
     }
 }
