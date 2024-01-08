8e3d7b20e23dd4fd16c3d290c265bba9ca93d053
Ohleesang <jammmmed@gmail.com>
2024-01-08 15:54:13 +0900

    Fix : 코드 정리 및 해당 스크롤 위치 저장 하여 다시 보여 주기

--- app/src/main/java/com/example/applemarketapp/MainActivity.kt
+++ app/src/main/java/com/example/applemarketapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,11 +16,18 @@ import com.example.applemarketapp.data.Item
 import com.example.applemarketapp.data.ItemList
 import com.example.applemarketapp.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val recyclerView by lazy{binding.itemListRv}
+    private val linearLayoutManager = LinearLayoutManager(this)
+
     private lateinit var resultDetailLauncher: ActivityResultLauncher<Intent>
+
+    private var savedPosition: Int = 0
 
     //알림
     private val notification = Notification(this)
@@ -31,74 +37,41 @@ class MainActivity : AppCompatActivity() {
         //더미 데이터 최초 생성
         setDummyData("dummy_data.xlsx")
 
-        //뷰를 생성
-        createView()
-
-        //결과값 처리
-        resultDetailLauncher()
-
-    }
-
-    //데이터 결과 처리
-    private fun resultDetailLauncher() {
-        resultDetailLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    //데이터 받기
-                    val data = result.data
-                    val position = data?.getIntExtra("position", 0)
-                    val item = data?.getParcelableExtra<Item>("item")
-
-                    //데이터 처리
-                    if (position != null && item != null) ItemList.value[position] = item
-                    createView()
-                }
-            }
-    }
-
-    //뷰를 생성 하는 부분
-    private fun createView() {
-
-        setContentView(binding.root)
-
-        //RecyclerView 생성 하기
-        setRecyclerView(binding.itemListRv, ItemList.value)
-
-        //아이템 리스트 클릭시 이벤트 처리
-        setItemOnclick(binding.itemListRv.adapter)
 
         //알림 채널 생성
         notification.createNotificationChannel()
 
-        //벨 클릭시 이벤트 처리
-        setOnTouchBell()
+        //뷰를 생성
+        createView()
 
-        //플로핑 버튼 클릭시 처리
-        setOnTouchFloating()
+        //콜백 함수들 정의
+        setOnCallBackFunction()
 
     }
 
-    private fun setOnTouchFloating() {
-        val btn = binding.floatingBtnIv
-        btn.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    btn.setImageResource(R.drawable.img_arrow_up_clicked)
-                    //처음 위치로
-                    binding.itemListRv.smoothScrollToPosition(0)
-                }
 
-                MotionEvent.ACTION_UP -> {
-                    btn.setImageResource(R.drawable.img_arrow_up)
-                    //이벤트 발생!
-                }
-            }
-
-            true
-        }
+    //화면이 재생성 되었을때
+    override fun onResume() {
+        super.onResume()
+        createView()
     }
 
-    //뒤로가기 버튼 눌렀을때,
+    //액티비티 가 넘겨가질때 현재 액티비티는 정지상태
+    override fun onPause() {
+        super.onPause()
+
+        // 해당 액티비티의 위치를 찾기
+        binding.itemListRv.onScrollStateChanged(RecyclerView.SCROLL_STATE_IDLE)
+
+    }
+
+    // 인스턴스 값 넣기
+    private fun setDummyData(fileName: String) {
+        //인스턴스 값 넣기
+        ItemList.setDummyData(this, fileName)
+    }
+
+    // 뒤로가기 버튼 눌렀을때,
     override fun onBackPressed() {
 
         //다이얼로그 실행
@@ -110,6 +83,7 @@ class MainActivity : AppCompatActivity() {
             setPositiveButton("확인") { dialog, _ ->
                 dialog.dismiss()
                 super.onBackPressed()
+
             }
             setNegativeButton("취소") { dialog, _ ->
                 dialog.dismiss()
@@ -119,35 +93,87 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setDummyData(fileName: String) {
-        //인스턴스 값 넣기
-        ItemList.setDummyData(this, fileName)
+
+
+
+    //뷰를 생성 하는 부분
+    private fun createView() {
+
+        setContentView(binding.root)
+
+        //RecyclerView 생성 하기
+        setRecyclerView(ItemList.value)
+
+        //저장된 위치에 보여주기
+        binding.itemListRv.scrollToPosition(savedPosition)
+
     }
 
-    private fun setRecyclerView(recyclerView: RecyclerView, mList: MutableList<Item>) {
-        val linearLayoutManager = LinearLayoutManager(this)
+
+    //RecyclerView 생성
+    private fun setRecyclerView(mList: MutableList<Item>) {
+
+
         recyclerView.layoutManager = linearLayoutManager
 
         val adapter = ItemAdapter(mList)
         recyclerView.adapter = adapter
 
 
-        //스크롤 변동될때 생기는 메소드
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                // 스크롤 발생 시 호출되는 메서드
-                // dx: 수평 스크롤의 변화량, dy: 수직 스크롤의 변화량
-                val floatingBtn = binding.floatingBtnIv
-                val isFirstItemVisible =
-                    linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0
-                if (isFirstItemVisible)
-                    floatingBtn.visibility = View.INVISIBLE
-                else floatingBtn.visibility = View.VISIBLE
-            }
-        })
+        // 인터페이스 정의
+        //아이템 리스트 클릭시 이벤트 처리
+        setItemOnclick(binding.itemListRv.adapter)
+
+
 
     }
 
+    //콜백 함수들 정의
+    private fun setOnCallBackFunction() {
+
+        //아이템 리스트 클릭시 이벤트 처리
+        setItemOnclick(binding.itemListRv.adapter)
+
+        //스크롤 변동시 이벤트 처리
+        setRecyclerViewOnScrolled()
+
+        //벨 클릭시 이벤트 처리
+        setOnTouchBell()
+
+        //플로핑 버튼 클릭시 처리
+        setOnTouchFloating()
+
+        //결과값 받는 이벤트 처리
+        resultDetailLauncher()
+
+    }
+
+
+
+
+    // 플로팅 버튼 터치시 처리
+    private fun setOnTouchFloating() {
+        val floatingBtn = binding.floatingBtnIv
+        floatingBtn.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    floatingBtn.setImageResource(R.drawable.img_arrow_up_clicked)
+
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    floatingBtn.setImageResource(R.drawable.img_arrow_up)
+                    //처음 위치로
+                    binding.itemListRv.smoothScrollToPosition(0)
+                    binding.floatingBtnIv.visibility = View.INVISIBLE
+                }
+            }
+
+            true
+        }
+    }
+
+    // 리사이클 뷰의 자식들 클릭했을때 처리
     private fun setItemOnclick(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?) {
         (adapter as ItemAdapter).itemClick = object : ItemAdapter.ItemClick {
             override fun onClick(view: View, position: Int) {
@@ -175,6 +201,7 @@ class MainActivity : AppCompatActivity() {
                         ItemList.deleteData(position)
 
                         //데이터가 변경되었으면 화면을 다시 구성
+
                         createView()
 
                         dialog.dismiss()
@@ -189,6 +216,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    // 스크롤 변동될때 생기는 메소드
+    private fun setRecyclerViewOnScrolled(){
+
+        //스크롤 변동될때 생기는 메소드
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                // 스크롤 발생 시 호출되는 메서드
+                // dx: 수평 스크롤의 변화량, dy: 수직 스크롤의 변화량
+
+
+                // 플로팅 버튼 표시 여부 지정
+                val floatingBtn = binding.floatingBtnIv
+                val isFirstItemVisible =
+                    linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0
+                if (isFirstItemVisible)
+                    floatingBtn.visibility = View.INVISIBLE
+                else floatingBtn.visibility = View.VISIBLE
+
+            }
+
+            //스크롤 상태에 따라 기능실행
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    savedPosition = linearLayoutManager.findFirstCompletelyVisibleItemPosition()
+                }
+
+            }
+        })
+
+    }
+
+
+    // 알람 버튼 터치시 처리
     private fun setOnTouchBell() {
         val bellView = binding.bellIv
         bellView.setOnTouchListener { _, event ->
@@ -206,4 +268,25 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
+    //데이터 결과 처리
+    private fun resultDetailLauncher() {
+        resultDetailLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    //데이터 받기
+                    val data = result.data
+                    val position = data?.getIntExtra("position", 0)
+                    val item = data?.getParcelableExtra<Item>("item")
+
+                    //데이터 처리
+                    if (position != null && item != null) ItemList.value[position] = item
+
+
+                }
+            }
+    }
+
+
 }
+
